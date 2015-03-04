06ec9bbf6993aecbf2b9b696408daac1bca9bd97
nath@home <nathdwek@ulb.ac.be>
2015-03-04 21:55:39 +0100

    init commit

---
+++ propulsion.X/main.c
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.c
+ * Author: labo
+ *
+ * Created on 4 mars 2015, 15:08
+ */
+#include   <p33FJ128MC804.h>
+
+/*
+ * 
+ */
+void configPWM(){
+    RPOR6bits.RP13R = 0b10010;//Lie la patte rightPWM à l'OC1
+    RPOR9bits.RP18R = 0b10011;//Lie la patte leftPWM à l'OC2
+    OC1CONbits.OCM=0b110;//Mode PWM no fault protection
+    OC2CONbits.OCM=0b110;//Mode PWM no fault protection
+    T2CONbits.TCKPS=0b01;//Prescaler 8
+    PR2=50000;//Periode du timer 2=50000*(1/40*1000000)*8=1/100s
+    OC1CONbits.OCTSEL=0;
+    OC2CONbits.OCTSEL=0;//Les deux Output compare utilisent le timer 2
+    T2CONbits.TON =1;//Lance Timer2
+}
+
+void configQEI(){
+    QEI1CONbits.QEIM=0b110;
+    QEI2CONbits.QEIM=0b110;//MODE 4X sans remise a zero par l'index
+    RPINR14bits.QEA1R = 0b10011;
+    RPINR14bits.QEB1R = 0b10110;
+    RPINR16bits.QEA2R = 0b11000;
+    RPINR16bits.QEB2R = 0b11001;
+}
+
+
+void main() {
+    configPWM();
+    //configQEI();
+    OC1RS=10000;//2ms
+    OC2RS=5000;//-(-2)ms
+    while(1){
+        
+    }
+}
+
