619178a21ac5448ec292fc771726b3188b3efa78
nathdwek <nathdwek@users.noreply.github.com>
2015-03-05 09:12:07 +0100

    Update main.c

--- propulsion.X/main.c
+++ propulsion.X/main.c
@@ -4,14 +4,14 @@
  *
  * Created on 4 mars 2015, 15:08
  */
-#include   <p33FJ128MC804.h>
+#include <p33FJ128MC804.h>
 #include <init.h>
 
 void main() {
     configPWM();
     //configQEI();
     OC1RS=10000;//Pulse largeur 2ms
-    OC2RS=5000;//Pulse largeur 1ms => tension = -Vss mais moteur retourné=>OK
+    OC2RS=5000;//Pulse largeur 1ms => tension = -Vcc mais moteur retourné=>OK
     while(1){
         
     }
