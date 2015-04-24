415f078e0b34d1be590e339a579a2f84b6a46462
labo <us>
2015-04-24 11:59:34 +0200

    mplab is dumb

--- communication.X/ADC.c
+++ communication.X/ADC.c
@@ -7,5 +7,11 @@ void initADC(){
     AD1CON3bits.ADCS = 105;
     AD1PCFGL = 0xFFFF; //on met tout en digital
     AD1PCFGLbits.PCFG12 = 0; //AN12 c'est la patte 23, celle qu'on met en analogique
+    AD1CSSL = 0x000B;//il va scanner la 12e entrée
+    AD1CON1bits.ASAM = 1;
+    AD1CON1bits.SSRC = 0b010;
+    AD1CON1bits.FORM = 0;
+    
+
 
 }
